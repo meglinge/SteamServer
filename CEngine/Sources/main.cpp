741927bc15335b6475b0013b736b1798a1d7c428
meglinge <qh984963655@outlook.com>
2023-08-14 17:33:08 +0800

    添加了一些vcpkg库

--- CEngine/Sources/main.cpp
+++ CEngine/Sources/main.cpp
@@ -1,7 +1,10 @@
 #include <cstdio>
 #include <spdlog/spdlog.h>
+#include <mimalloc.h>
+#include <GLFW/glfw3.h>
 int main() {
-     printf("Hello, World!\n");
-     spdlog::info("Welcome to spdlog!");
+     int i = 10;
+     i++;
+     spdlog::info("Welcome to spdlog!{}", i);
      return 0;
 }
